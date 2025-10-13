c0ca62e8ec890e3b120082ebc72b87117b8bf8c2
chinmayd1 <chinmyay.minskole@gmail.com>
2025-10-13 21:27:55 +0530

    added

--- day9.py
+++ day9.py
@@ -45,3 +45,48 @@ print("1232".isdecimal()) # isdigit()
 print("a".isalnum())
 print('2'.isalnum())
 print("2q#".isalnum())
+txt = "python programming"
+print(txt.find('pro'))
+print("hello".islower())
+print("Hello".lower())
+print("HELLO".isupper())
+print("Hello".isupper())
+# isSpace()
+print(" ".isspace())
+print(". ".isspace())
+
+# istile()
+txt3 = "My Name is Chinmay"
+print(txt3.istitle())
+
+
+# join()
+info  = ["chinmay","deshpande","7709191441"]
+# chinmay-deshpande-7709192441
+print("@".join(info))
+
+# chinmay@deshpande@7709191441 ----> ["chinmay","deshpande",7709192441]
+
+txt5 = "hi"
+print(txt5.ljust(5,"."))
+print(txt5.rjust(5,"."))
+txt6 = "h1"
+print(txt6.center(6,'.'))
+
+
+text7 = "chinmay@deshpande@7709191441"
+# ["chinmay","deshpande",7709192441]
+# []"chinmay","deshpande","7709191441"]
+print(text7.split("@"))
+
+
+txt8 =  " goa "
+print(len(txt8))
+print(len(txt8.rstrip()))
+
+txt9 =  " goa"
+print(len(txt9))
+print(len(txt9.lstrip()))
+
+txt9 =  " goa "
+print(len(txt9.strip()))
