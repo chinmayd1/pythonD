37a493f1ce608b20d2c36cd4a462329e0238a730
chinmayd1 <chinmyay.minskole@gmail.com>
2025-10-29 19:24:04 +0530

    added

---
+++ 7pm/day1.py
@@ -0,0 +1,41 @@
+x = 100 
+print(x)
+x = 200
+print(x)
+
+score = 0
+print(0)
+score = 400
+print(score)
+
+# Arithmetic operation
+
+s = 10
+t = 5
+
+print(s+t)
+print(s-t)
+print(s*t)
+print(s/t)
+print(s%t)
+
+
+q1 = 8
+q2 = 4 
+
+print(q1 + q2)
+print(q1 - q2)
+print(q1 * q2)
+print(q1 / q2)
+print(q1 % q2)
+
+
+def Calculator(x,y):
+    print(x+y)
+    print(x-y)
+    print(x*y)
+    print(x/y)
+    print(x%y)
+Calculator(12,3)
+Calculator(4,2)
+
