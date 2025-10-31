3f297d70d685e9ceb1b19a67600327adc3f6708c
chinmayd1 <chinmyay.minskole@gmail.com>
2025-10-31 19:24:50 +0530

    added

--- 7pm/day2.py
+++ 7pm/day2.py
@@ -1,60 +1,186 @@
-# x = 10
-# print(x)
-# x = 300
-# print(x)
+# # x = 10
+# # print(x)
+# # x = 300
+# # print(x)
 
-# # Arithmetic operation 
+# # # Arithmetic operation 
 
-# a = 10
-# b = 5
+# # a = 10
+# # b = 5
 
-# print(a+b)
-# print(a-b)
-# print(a*b)
-# print(a/b)
-# print(a%b)
+# # print(a+b)
+# # print(a-b)
+# # print(a*b)
+# # print(a/b)
+# # print(a%b)
 
-# s = 8
-# t = 4
+# # s = 8
+# # t = 4
 
-# print(s+t)
-# print(s-t)
-# print(s*t)
-# print(s/t)
-# print(s%t)
-# print(23 % 5)
+# # print(s+t)
+# # print(s-t)
+# # print(s*t)
+# # print(s/t)
+# # print(s%t)
+# # print(23 % 5)
 
-# # CEO ---> Office sham 
-# def Calculator(x,y):
+# # # CEO ---> Office sham 
+# # def Calculator(x,y):
+# #     print(x+y)
+# #     print(x-y)
+# #     print(x*y)
+# #     print(x/y)
+# #     print(x%y)
+# # Calculator(12,3)
+# # Calculator(4,2)
+
+# # function without parameter and without return type
+
+# def CalA():
+#     print(9+9)
+# CalA()
+# CalA()
+# CalA()
+
+# # function with parameter and without return type 
+# def CalB(x,y):
 #     print(x+y)
-#     print(x-y)
-#     print(x*y)
-#     print(x/y)
-#     print(x%y)
-# Calculator(12,3)
-# Calculator(4,2)
+# CalB(12,3)
+# CalB(4,2)
+# CalB(2,1)
 
-# function without parameter and without return type
+# # function with parameter and with return
+# # 100 given , 100 show
+# # 100 + 100 , 100 - 50 , 100 * 0.10 , 100/10
+# def CalC(x,y):
+#     return x + y
 
-def CalA():
-    print(9+9)
-CalA()
-CalA()
-CalA()
+# e = CalC(12,3)
+# print(e)
+# print(e + e) 
 
-# function with parameter and without return type 
-def CalB(x,y):
+
+
+
+x = 10
+print(x)
+x = 1000
+print(x)
+
+
+
+# Arithmetic 
+
+x = 10
+y = 5
+
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(23 % 5)
+
+
+d = 9
+e = 3
+
+print(d+e)
+print(d-e)
+print(d*e)
+print(d/e)
+print(d%e)
+
+def CalA(x,y):
     print(x+y)
-CalB(12,3)
-CalB(4,2)
-CalB(2,1)
+    print(x-y)
+    print(x*y)
+    print(x/y)
+    print(x%y)
 
-# function with parameter and with return
-# 100 given , 100 show
-# 100 + 100 , 100 - 50 , 100 * 0.10 , 100/10
-def CalC(x,y):
+CalA(12,3)
+CalA(10,5)
+
+
+# day 2
+
+# function without parameter and without return type 
+
+def addA():
+    print(9+9)
+
+addA()
+addA()
+addA()
+
+
+# function with parameter and with return type 
+def addB(x,y):
+    print(x+y)
+
+addB(12,3)
+addB(12,1)
+addB(12,4)
+
+# function with parameter and with return type 
+def addC(x,y):
     return x + y
+r = addC(12,3)
+print(r)
+print(r+r)
+print(r-10)
 
-e = CalC(12,3)
-print(e)
-print(e + e) 
+# day 3
+
+# introvert      extrovert 
+# calm            loud
+# less social     more outing
+# less outing     more social
+
+# Human
+# Properties - height , weight , age
+# Methods - talk() , eating
+
+# Vehicle 
+# Properties - color , type , companyName
+# Methods - start() , stop()
+
+# Bank acc
+# Properties - bal , type , IFSC code , accNo 
+# Methods - withdrawl() , deposit()
+
+q = 10
+print(q)
+print(type(q))
+# 10, -10 ,0
+
+q2 = 10.5
+print(q2)
+print(type(q2))
+# 10.5 , -10.5 , 0.34
+
+q3 = True
+print(q3)
+print(type(q3))
+# True or False
+
+q4 = "chinmay"
+print(q4)
+print(type(q4))
+# 'c',"C","234","c45"," ","#@$FGSDGDG"
+
+# comparison operation 
+# enity < entity ---> comparison --> boolean --> True or False
+# < , > , <= , >= , == , != 
+print(3 > 2)
+print(3 < 2)
+print(3 == 3)
+print(3 != 3)
+print(3 != 2)
+print(3 == 2)
+print(3 >= 1)
+print(3 <= 1)
+print(3 >= 3)
+print(3 <= 3)
+
