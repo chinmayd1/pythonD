1e59a457c4ed51a9cfe054e92c03e520e6a69670
chinmayd1 <chinmyay.minskole@gmail.com>
2025-11-10 19:20:48 +0530

    added

---
+++ 7pm/day6.py
@@ -0,0 +1,58 @@
+# loops
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+# program 2
+# for loop and while loop 
+# range(startIndex,endIndex(not inclued),stepSize)
+#range(5)  # startIndex -0 , endindex- 5 , stepSize - 1
+#range(1,5) # startIndex - 1 , endindex - 5 , stepSize - 1
+#range(2,21,2) # startIndex - 2 , endIndex - 21 , stepSize - 2
+
+# print - 0 to 4
+for x in range(5):
+    print(x)
+ 
+# print 1 to 5
+for x in range(1,6):
+    print(x)
+
+# print "hello"  3 times 
+for x in range(3):
+    #print(x)
+    print("hello")
+
+# print table of 2
+# 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 
+for x in range(2,21,2):
+    print(x)
+
+#table of 3 
+for x in range(3,31,3):
+    print(x)
+
+# table of 5
+for x in range(5,51,5):
+    print(x)
+
+# print 5 to 1 in reverse 
+for x in range(5,0,-1):
+    print(x)
+
+for x in range(10,0,-1):
+    print(x)
+
+for x in range(20,1,-2):
+    print(x)
+
+for x in range(30,3,-3):
+    print(x)
+
+# for loop with break 
+
+# for loop with continue
+
+# while loop
